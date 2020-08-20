1ac77c5a70b3e143e1460cc116ce4e93858ddb35
Wojciech Gołda <w.golda@praktyki.ideo.pl>
2020-08-20 14:08:13 +0200

    zadanie w React.js

--- sekcja 4 proste projkty w React/zadanie dla mnie losowanie wrozby/App.js
+++ sekcja 4 proste projkty w React/zadanie dla mnie losowanie wrozby/App.js
@@ -6,33 +6,36 @@ class App extends React.Component {
   };
   handleRandPrediction = () => {
     let rand = Math.floor(Math.random() * this.state.predictions.length);
-
     this.setState({
       view: this.state.predictions[rand],
     });
   };
   handleAddPrediction = () => {
-    this.setState((prev) => ({
-      predictions: [...prev.predictions, this.state.adding],
-      adding: "",
-    }));
-    alert("aktualnie mamy wróżby: " + this.state.predictions);
+    if (this.state.adding === "") {
+      alert("nie ma nic w wpisywaniu");
+    } else {
+      this.setState((prev) => ({
+        predictions: [...prev.predictions, this.state.adding],
+        // dokladnie kopia tablicy
+        adding: "",
+      }));
+      alert("aktualnie mamy wróżby: " + this.state.predictions);
+    }
   };
   handleChangeAdd = (e) => {
     this.setState({
       adding: e.target.value,
     });
   };
-
   render() {
     // console.log();
     return (
       <>
         <button onClick={this.handleRandPrediction}>Zobacz wróżbę</button>
         <br />
-        <input onChange={this.handleChangeAdd} />
+        <input value={this.state.adding} onChange={this.handleChangeAdd} />
         <button onClick={this.handleAddPrediction}>Dodaj wróżbe</button>
-        <h1>{this.state.view}</h1>
+        {this.state.view != "" ? <h1>{this.state.view}</h1> : null}
       </>
     );
   }
